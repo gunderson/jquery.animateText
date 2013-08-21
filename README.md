19ae2677eb916ba60b903372fe5279f3e15c55d1
Patrick Gunderson <patrick@toolofna.com>
2013-08-20 18:37:01 -0700

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# jQuery Text Animation plugin
+
+jQ
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/gunderson/jquery-textanimation/master/dist/jquery.textanimation.min.js
+[max]: https://raw.github.com/gunderson/jquery-textanimation/master/dist/jquery.textanimation.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/textanimation.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/animateText.js
@@ -0,0 +1,161 @@
+/*
+ * jquery.animateText
+ * 
+ *
+ * Copyright (c) 2013 Patrick Gunderson
+ * Licensed under the MIT license.
+ */
+
+(function ($) {
+
+  // Static method.
+  $.animateText = function (el, options) {
+    // Override default options with passed-in options.
+    var $el = $(el);
+    var $holder;
+    var _this = this;
+    this.options = $.extend({}, $.animateText.options, options);
+
+    this.init = function(){
+      _this.options.originalString = $el.text();
+      $holder = $("<div class='at-holder'>")
+        .css({
+          position: "relative"
+        })
+        .append(_this.options.originalString);
+      $el.empty().append($holder);
+      wrapWords($holder);
+      wrapCharacters($holder);
+      prepCSS($holder);
+    };
+
+    function wrapWords($holder){
+      var words = _this.options.originalString.split(' ');
+      $.each(words, function(i, word){
+        words[i] = "<span class='at-word'>"+ word +"</span>";
+      });
+      $holder.html(words.join("<span class='at-character'>&nbsp;</span>"));
+      return $holder;
+    }
+
+    function wrapCharacters($holder){
+      var $words = $holder.find('.at-word');
+      $.each($words, function(i, word){
+        var $word = $(word);
+        var characters = $word.text().split("");
+        $.each(characters, function(j, character){
+          characters[j] = "<span class='at-character'>"+ character +"</span>";
+        });
+        $word.html(characters.join(""));
+      });
+      return $holder;
+    }
+
+    function prepCSS($holder){
+      $holder.find("span").each(function(){
+        var $this = $(this);
+        $this.css({
+          display: "inline-block",
+          position: "relative"
+        });
+      });
+    }
+
+    function checkComplete(iterator){
+      if (iterator === _this.options.length - 1){
+        $el.trigger("animateText:complete");
+      }
+    }
+
+    this.from = function(options){
+      var $characters = $holder.find('.at-character');
+      _this.options.length = $characters.length;
+      $characters.each(function(i, character){
+        var $character = $(character);
+        var originalCSS = {};
+        $.each(options.css, function(i){
+          originalCSS[i] = $character.css(i);
+        });
+        $character.css(options.css);
+        setTimeout(function(){
+          $character.transition(originalCSS, options.duration, 'easeOutQuad', function(){
+            checkComplete(i);
+          });
+        }, i * options.delay);
+      });
+      return _this;
+    };
+
+    this.to = function(options){
+      var $characters = $holder.find('.at-character');
+      _this.options.length = $characters.length;
+      $characters.each(function(i, character){
+        var $character = $(character);
+        setTimeout(function(){
+          $character.transition(options.css, options.duration, 'easeOutQuad', function(){
+            checkComplete(i);
+          });
+        }, i * options.delay);
+      });
+      return _this;
+    };
+
+    this.hide = function(){
+      $(".at-character").hide();
+    };
+
+    this.init();
+
+    // Return something awesome.
+    return this;
+  };
+
+  // Static method default options.
+  $.animateText.options = {
+    delay: 20,
+    duration: 500,
+    originalString: ''
+  };
+
+  // Collection method.
+  $.fn.animateText = function (options) {
+    return this.each(function () {
+      var $this = $(this);
+      var anim;
+      if (!(anim = this.animateText)){
+        //create a new animateText instance
+        this.animateText = anim = new $.animateText(this, options);
+      }
+
+      //handle parametric commands and updated options
+      if(typeof options === 'object'){
+        $.extend(anim.options, options);
+        options = $.extend({}, anim.options);
+
+        if (options.from){
+          delete anim.options.from;
+          options = $.extend({css: options.from}, anim.options);
+          anim.from(options);
+        }
+
+        if (options.to){
+          delete anim.options.to;
+          options = $.extend({css: options.to}, anim.options);
+          anim.to(options);
+        }
+        
+        if (options.command){
+          // allow making setting changes and a command in one call
+          options = options.command;
+        }
+      }
+
+      //execute standalone commands
+      if (typeof options === "string"){
+        
+        return $this;
+      }
+    });
+  };
+
+}(jQuery));
